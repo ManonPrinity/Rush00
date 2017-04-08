3fbf68a87b041ea2ff5f45550fe036ccd79d38b9
Noblet MANON <nmanon@e1r9p19.42.fr>
2017-04-08 05:45:10 +0200

    initial commit

---
+++ index.php
@@ -0,0 +1,12 @@
+
+<?php
+	include("templates/header.php");
+	include("templates/navbar.php");
+	include("templates/inscription_form.php");
+	include("templates/slider.php");
+	include("templates/footer.php");
+
+?>	
+	</body>
+	<script type="text/javascript" src="scripts/slid_show.js"></script>
+</html>
