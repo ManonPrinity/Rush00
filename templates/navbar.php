ce186ee830ba79271c5363c4a55b1b36432025c2
Noblet MANON <nmanon@e1r7p3.42.fr>
2017-04-09 21:11:41 +0200

    annim

--- templates/navbar.php
+++ templates/navbar.php
@@ -3,6 +3,7 @@
 		<a href="index.php"><input type="button" value="SignUp"></a>
 		<a href="connection.php"><input type="button" value="SignIn"></a>
 		<a href="page_produit.php"><input type="button" value="Nos Produits"></a>
-		<a href="page_produit.php"><img src="images/pokebag.svg"></a>
+		<a href="#"><input type="button" value="XXX"></a>
+		<a href="panier_p.php"><img src="images/pokebag.svg"></a>
 	</div>
 </section>
